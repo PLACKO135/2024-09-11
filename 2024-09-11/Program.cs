4d2de0e237c195a65f5b8f4f8c43372f5213d422
P.Laszlo <70635967+PLACKO135@users.noreply.github.com>
2024-09-11 09:19:11 +0200

    Dunno why not working

--- 2024-09-11/Program.cs
+++ 2024-09-11/Program.cs
@@ -1,1 +1,53 @@
-﻿
+﻿//1
+if (args.Length % 2 == 0 || args.Length <= 2)
+{
+    Console.WriteLine("Nem megfelelő argumentumszám!");
+}
+else
+{
+    int middleIndex = args.Length / 2;
+    int num1 = Convert.ToInt32(args[middleIndex - 1]);
+    int num2 = Convert.ToInt32(args[middleIndex + 1]);
+
+    if (num2 > num1)
+    {
+        Console.WriteLine($"Eredmény:{Math.Pow(num1, num2 / num1)}");
+    }
+    else
+    {
+        Console.WriteLine($"Eredmény:{Math.Pow(num2, num1 / num2)}");
+    }
+}
+
+
+//2
+List < string > szavak= File.ReadAllLines("szavak.txt").ToList();
+char[] maganhangzok = ['A','E','I','U','O'];
+int szotagok=0;
+int szotagokTobbMintNegy = 0;
+int legnagyobbszotag = 0;
+
+foreach (var item in szavak)
+{
+    foreach (var j in maganhangzok)
+    {
+        if (item.Contains(maganhangzok[j]))
+        {
+            szotagok += 1;
+        }
+        if (szotagok>4)
+        {
+            szotagokTobbMintNegy += 1;
+        }
+        if (legnagyobbszotag<szotagok)
+        {
+            legnagyobbszotag = szotagok;
+        }
+        szotagok = 0;
+    }
+   
+}
+Console.WriteLine($"A több mint négy szótagból álló szavak száma: {szotagokTobbMintNegy}");
+Console.WriteLine($"A legnagyobb szótagszám: {legnagyobbszotag}");
+
+//3
